413b04442c7ea28f986d0619ba2500d42322089d
rodeloescueta <cxcxcxtest@gmail.com>
2019-05-31 13:02:19 +0800

    for github pages

---
+++ dist/precache-manifest.98c8b1eb9dd8c10fa5e3573c26aad7be.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-konva-container/robots.txt"
+  },
+  {
+    "revision": "f4b61bc92e3d40f1f361",
+    "url": "/vue-konva-container/js/chunk-vendors.9eddbd09.js"
+  },
+  {
+    "revision": "3be0eecf0525630c0409",
+    "url": "/vue-konva-container/js/chunk-06b4b0dd.7e4b3b5f.js"
+  },
+  {
+    "revision": "9c96c8ab98a956eee725",
+    "url": "/vue-konva-container/js/app.afc60ac8.js"
+  },
+  {
+    "revision": "d35a51f4e6096878d35a",
+    "url": "/vue-konva-container/js/about.6baa7db9.js"
+  },
+  {
+    "revision": "a6bee290178bd619b3a58b6ae1a8efdb",
+    "url": "/vue-konva-container/index.html"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/vue-konva-container/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "f4b61bc92e3d40f1f361",
+    "url": "/vue-konva-container/css/chunk-vendors.a01e2b95.css"
+  },
+  {
+    "revision": "3be0eecf0525630c0409",
+    "url": "/vue-konva-container/css/chunk-06b4b0dd.f4953634.css"
+  }
+];
---
+++ dist/service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/vue-konva-container/precache-manifest.98c8b1eb9dd8c10fa5e3573c26aad7be.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "res3"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
